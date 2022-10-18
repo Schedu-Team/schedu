4dbd4b9819b5bf7acf6647bdc0a887ef8f4d52d4
Alex Kovrigin <a.kovrigin0@gmail.com>
2022-10-18 22:56:05 +0200

    first version site

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 
 # Build Web (in the future will be more complicated)
 mkdir -p dist
-cp -r web/src/* dist/
+cd web
+npm i
+npm run build
+cd ..
+cp web/index.html dist
+cp -r web/assets dist
 
 # Add git commit to the dist
 git rev-parse HEAD > dist/version.txt
