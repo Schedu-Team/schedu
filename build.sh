57becbf4d05237ec9bc12e5031d88afcd0ccd9ab
Alex Kovrigin <a.kovrigin0@gmail.com>
2022-10-20 19:36:55 +0200

    fix deploy script

--- build.sh
+++ build.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env sh
 
 # Build Web
-mkdir -p dist
-
 (
 cd web
 npm i
 npm run build
 )
 
-cp -r web/dist/ dist
+cp web/dist/* dist
 
 # Check file exists
-test -f dist/index.html
+test -f ./dist/index.html || exit 1
 
 # Add git commit to the dist
 git rev-parse HEAD > dist/version.txt
