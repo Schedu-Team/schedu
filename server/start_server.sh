cb26f3879793ff58f1a51042e26261e95c99dcd4
Mark Ipatov <markthehopeful@gmail.com>
2022-10-24 22:28:35 +0200

    Updated start_server.sh

--- server/start_server.sh
+++ server/start_server.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-# You should run this inside a tmux session
-
 export FLASK_APP=server.py
 PORT=${PORT=30566}
 SERVER_HOST=${SERVER_HOST="0.0.0.0"}
-flask run --host="$SERVER_HOST" --port="$PORT" &> .server.log
-
-# And then leave the session using ^-B d
-# TODO: Make the whole app start process run using just one (this) script
+nohup flask run --host="$SERVER_HOST" --port="$PORT" >.server.log &
