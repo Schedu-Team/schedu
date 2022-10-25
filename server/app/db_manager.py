260958996d0c21d10c152edfee3180451b46eaae
Mark Ipatov <markthehopeful@gmail.com>
2022-10-25 20:44:25 +0200

    Should work

--- server/app/db_manager.py
+++ server/app/db_manager.py
@@ -77,7 +77,7 @@ class DBManager:
     @with_connect
     def select_field(self, connection: mysql.connector.connection, table_name: str, column_name: str, column_value: str):
         cursor = connection.cursor()  # Cursor types are weird garbage
-        cursor.execute(f"SELECT * FROM `{table_name}` WHERE `{column_name}= %s`;", column_value)
+        cursor.execute(f"SELECT * FROM `{table_name}` WHERE `{column_name} = %s`;", (column_value,))
         records = list(cursor.fetchall())
         cursor.close()
         return records
