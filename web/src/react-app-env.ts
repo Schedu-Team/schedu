0f802ab17959fb97a572b5b82d6646272102d53a
Alex Kovrigin <a.kovrigin0@gmail.com>
2022-10-31 23:58:58 +0100

    [web] Fix endpoint environ

--- web/src/react-app-env.ts
+++ web/src/react-app-env.ts
@@ -1,5 +1,5 @@
 /// <reference types="react-scripts" />
 
 export const API_ENDPOINT =
-  process.env.ENDPOINT ??
+  process.env.REACT_APP_ENDPOINT ??
   "http://clabsql.clamv.jacobs-university.de:30566/api/v1";
